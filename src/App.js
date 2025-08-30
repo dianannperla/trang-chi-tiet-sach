23138a4e47d5fdf869b70a3d55f5f7d53810c387
dianannperla <waafirmelyza@hotmail.com>
2025-08-30 07:07:59 +0100

    feat: initial commit for src/App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="container">
+      <h1 className="mt-5">Trang Chi Tiết Sách</h1>
+      <p>Tìm kiếm và khám phá các cuốn sách yêu thích của bạn!</p>
+      {/* Thêm các thành phần tìm kiếm và hiển thị sách tại đây */}
+    </div>
+  );
+}
+
+export default App;
